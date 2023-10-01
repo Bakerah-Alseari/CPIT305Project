82a6931148e1f96c44a547312acb691f086ca923
Bakerah-Alseari <143954286+Bakerah-Alseari@users.noreply.github.com>
2023-10-01 13:54:10 +0300

    Add files via upload

---
+++ CafeManagementSystem.java
@@ -0,0 +1,24 @@
+public class CafeManagementSystem {
+    public static void main(String[] args) {
+        Cafe cafe = new Cafe();
+
+        // Adding menu items
+        cafe.addItem(new MenuItem("Coffee", 2.50));
+        cafe.addItem(new MenuItem("Tea", 1.75));
+        // Add more menu items as needed
+
+        // Creating an order
+        Order order = cafe.createOrder();
+
+        // Adding items to the order
+        order.addItem(new OrderItem(cafe.getMenu().get(0), 2)); // 2 coffees
+        order.addItem(new OrderItem(cafe.getMenu().get(1), 1)); // 1 tea
+
+        // Displaying the order
+        System.out.println("Order Items:");
+        for (OrderItem item : order.getOrderItems()) {
+            System.out.println(item.getMenuItem().getName() + " x" + item.getQuantity() + " - $" + item.getTotalPrice());
+        }
+        System.out.println("Total Price: $" + order.getTotalPrice());
+    }
+}
---
+++ project.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+class MenuItem {
+    private String name;
+    private double price;
+
+    public MenuItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+class OrderItem {
+    private MenuItem menuItem;
+    private int quantity;
+
+    public OrderItem(MenuItem menuItem, int quantity) {
+        this.menuItem = menuItem;
+        this.quantity = quantity;
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getTotalPrice() {
+        return menuItem.getPrice() * quantity;
+    }
+}
+
+class Order {
+    private List<OrderItem> orderItems;
+
+    public Order() {
+        this.orderItems = new ArrayList<>();
+    }
+
+    public void addItem(OrderItem item) {
+        orderItems.add(item);
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public double getTotalPrice() {
+        double total = 0;
+        for (OrderItem item : orderItems) {
+            total += item.getTotalPrice();
+        }
+        return total;
+    }
+}
+
+class Cafe {
+    private List<MenuItem> menu;
+
+    public Cafe() {
+        this.menu = new ArrayList<>();
+    }
+
+    public void addItem(MenuItem item) {
+        menu.add(item);
+    }
+
+    public List<MenuItem> getMenu() {
+        return menu;
+    }
+
+    public Order createOrder() {
+        return new Order();
+    }
+}
+
