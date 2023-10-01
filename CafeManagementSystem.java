eb249e926910b2674c047b97dd796e65c5e172a0
Bakerah-Alseari <143954286+Bakerah-Alseari@users.noreply.github.com>
2023-10-01 19:12:29 +0300

    Update CafeManagementSystem.java

--- CafeManagementSystem.java
+++ CafeManagementSystem.java
@@ -4,7 +4,7 @@ public class CafeManagementSystem {
     public static void main(String[] args) {
         Cafe cafe = new Cafe();
 
-        FileReader text=new FileReader("menu.txt");
+        FileReader text=new FileReader("MENU.txt");
         BufferedReader readMenu;
 
         readMenu= new BufferedReader(text);
@@ -15,17 +15,17 @@ public class CafeManagementSystem {
         // Add more menu items as needed
 
         // Creating an order
-        Order order = cafe.createOrder();
+        //Order order = cafe.createOrder();
 
         // Adding items to the order
-        order.addItem(new OrderItem(cafe.getMenu().get(0), 2)); // 2 coffees
-        order.addItem(new OrderItem(cafe.getMenu().get(1), 1)); // 1 tea
+        //order.addItem(new OrderItem(cafe.getMenu().get(0), 2)); // 2 coffees
+        //order.addItem(new OrderItem(cafe.getMenu().get(1), 1)); // 1 tea
 
         // Displaying the order
         System.out.println("Order Items:");
-        for (OrderItem item : order.getOrderItems()) {
-            System.out.println(item.getMenuItem().getName() + " x" + item.getQuantity() + " - $" + item.getTotalPrice());
+        //for (OrderItem item : order.getOrderItems()) {
+            //System.out.println(item.getMenuItem().getName() + " x" + item.getQuantity() + " - $" + item.getTotalPrice());
         }
-        System.out.println("Total Price: $" + order.getTotalPrice());
+        //System.out.println("Total Price: $" + order.getTotalPrice());
     }
 }
